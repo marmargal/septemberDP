d46818ca0bbe536ee21d7e9101122c81a94e24dc
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2018-07-14 17:17:16 +0200

    feat: clases dominio application, visa y creditCard

---
+++ 01-Acme_Inmigrant/Workspace/Acme-Inmigrant/src/main/java/domain/CreditCard.java
@@ -0,0 +1,79 @@
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+
+@Embeddable
+@Access(AccessType.PROPERTY)
+public class CreditCard {
+
+	// Attributes
+
+	private String holderName;
+	private String brandName;
+	private String number;
+	private Integer expirationMonth;
+	private Integer expirationYear;
+	private Integer CVV;
+
+	@NotBlank
+	public String getHolderName() {
+		return holderName;
+	}
+
+	public void setHolderName(String holderName) {
+		this.holderName = holderName;
+	}
+
+	@NotBlank
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	@CreditCardNumber
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	@Range(min = 1, max = 12)
+	public Integer getExpirationMonth() {
+		return expirationMonth;
+	}
+
+	public void setExpirationMonth(Integer expirationMonth) {
+		this.expirationMonth = expirationMonth;
+	}
+
+	@Min(0)
+	public Integer getExpirationYear() {
+		return expirationYear;
+	}
+
+	public void setExpirationYear(Integer expirationYear) {
+		this.expirationYear = expirationYear;
+	}
+
+	@Range(min = 100, max = 999)
+	public Integer getCVV() {
+		return CVV;
+	}
+
+	public void setCVV(Integer cVV) {
+		CVV = cVV;
+	}
+
+}
