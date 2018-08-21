84d2820e79fa414e38b9a106f9b181740d77b7c1
José Manuel Rojas Granado <jrojasgranado@gmail.com>
2018-08-21 19:25:36 +0200

    registry inn

---
+++ 04-Acme_Santiago/Workspace/Acme-Santiago/src/main/java/controllers/inkeeper/InnInkeeperController.java
@@ -0,0 +1,107 @@
+package controllers.inkeeper;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.InnService;
+import services.RegistrytService;
+import controllers.AbstractController;
+import domain.Inn;
+import domain.Registry;
+import forms.InnForm;
+
+@Controller
+@RequestMapping("/inn/inkeeper")
+public class InnInkeeperController extends AbstractController {
+
+	// Services -------------------------------------------------------------
+
+	@Autowired
+	private InnService innService;
+
+	@Autowired
+	private RegistrytService registryService;
+
+	// Constructors ---------------------------------------------------------
+
+	public InnInkeeperController() {
+		super();
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam(defaultValue = "0") final int innId) {
+		ModelAndView result;
+		Inn inn = null;
+
+		if (innId == 0) {
+			result = new ModelAndView("redirect:../../");
+
+		} else if (this.innService.findOne(innId) == null) {
+			result = new ModelAndView("redirect:../../");
+
+		} else {
+			inn = this.innService.findOne(innId);
+			InnForm innForm = new InnForm();
+			innForm.setAddress(inn.getAddress());
+			innForm.setBadge(inn.getBadge());
+			innForm.setCreditCard(inn.getCreditCard());
+			innForm.setEmail(inn.getEmail());
+			innForm.setId(inn.getId());
+			innForm.setName(inn.getName());
+			innForm.setPhoneNumber(inn.getPhoneNumber());
+			innForm.setVersion(inn.getVersion());
+			innForm.setWebSite(inn.getWebSite());
+			innForm.setInkeeper(inn.getInkeeper());
+			result = this.createEditModelAndView(innForm);
+			result.addObject("innForm", innForm);
+		}
+		return result;
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid InnForm innForm, final BindingResult binding) {
+		ModelAndView res;
+		if (binding.hasErrors())
+			res = this.createEditModelAndView(innForm, "inn.params.error");
+		else
+			try {
+				Inn inn = this.innService.findOne(innForm.getId());
+				Registry registry = this.registryService.create();
+				registry.setDate(innForm.getDate());
+				registry.setInn(inn);
+				registryService.save(registry);
+				res = new ModelAndView("redirect:../../");
+			} catch (final Throwable oops) {
+				res = this.createEditModelAndView(innForm, "inn.commit.error");
+			}
+		return res;
+	}
+
+	protected ModelAndView createEditModelAndView(final InnForm inn) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(inn, null);
+
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final InnForm inn,
+			final String message) {
+		ModelAndView result;
+
+		result = new ModelAndView("inn/inkeeper/register");
+		result.addObject("inn", inn);
+		result.addObject("message", message);
+		result.addObject("requestURI", "inn/inkeeper/register.do");
+
+		return result;
+	}
+
+}
---
+++ 04-Acme_Santiago/Workspace/Acme-Santiago/src/main/java/services/RegistrytService.java
@@ -0,0 +1,73 @@
+package services;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.RegistryRepository;
+import domain.Registry;
+
+@Service
+@Transactional
+public class RegistrytService {
+	// Managed repository -----------------------------------------------------
+
+	@Autowired
+	private RegistryRepository registytRepositoty;
+
+	// Supporting services ----------------------------------------------------
+
+	@Autowired
+	private InkeeperService inkeeperService;
+
+	// Constructor ------------------------------------------------------------
+
+	public RegistrytService() {
+		super();
+	}
+
+	// Simple CRUD methods ----------------------------------------------------
+
+	public Registry create() {
+		this.inkeeperService.checkAuthority();
+		Registry result;
+		Date moment;
+
+		result = new Registry();
+		moment = new Date(System.currentTimeMillis() - 1000);
+		result.setDate(moment);
+
+		return result;
+	}
+
+	public Collection<Registry> findAll() {
+		Collection<Registry> res;
+		res = this.registytRepositoty.findAll();
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Registry findOne(int id) {
+		Assert.isTrue(id != 0);
+		Registry res;
+		res = this.registytRepositoty.findOne(id);
+		Assert.notNull(res);
+		return res;
+	}
+
+	public Registry save(Registry registry) {
+		this.inkeeperService.checkAuthority();
+		Assert.notNull(registry);
+		Registry res;
+		res = this.registytRepositoty.save(registry);
+		return res;
+	}
+
+	// Other business method --------------------------------------------------
+
+}
