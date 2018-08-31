d00b6e7801ff24e86966459aa424fdedb7e53a2f
MarioShez <32955600+MarioShez@users.noreply.github.com>
2018-08-31 17:23:07 +0200

    Populate

--- 06-Hackathon/Workspace/Acme-AnimalShelter/src/main/java/domain/MedicalReport.java
+++ 06-Hackathon/Workspace/Acme-AnimalShelter/src/main/java/domain/MedicalReport.java
@@ -7,6 +7,7 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -15,7 +16,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
@@ -97,7 +97,7 @@ public class MedicalReport extends DomainEntity{
 	}
 
 	@Valid
-	@OneToOne(optional=false)
+	@ManyToOne(optional=false)
 	public Veterinary getVeterinary() {
 		return veterinary;
 	}
--- 06-Hackathon/Workspace/Acme-AnimalShelter/src/main/java/domain/Pet.java
+++ 06-Hackathon/Workspace/Acme-AnimalShelter/src/main/java/domain/Pet.java
@@ -37,8 +37,7 @@ public class Pet extends DomainEntity{
 	private Boolean chip;
 	
 	@NotBlank
-//	@Pattern(regexp = "^((DOG)|(CAT)|(BIRD))$")
-//	@Pattern(regexp = "^DOG|CAT|BIRD$")
+	@Pattern(regexp = "^((DOG)|(CAT)|(BIRD))$")
 	public String getType() {
 		return type;
 	}
