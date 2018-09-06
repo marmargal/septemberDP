8ffde3365e0d0e74307e3feb0f8fc539d5246196
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2018-09-06 20:26:16 +0200

    feat: application sobre un pet

--- 06-Hackathon/Workspace/Acme-AnimalShelter/src/main/java/services/ApplicationService.java
+++ 06-Hackathon/Workspace/Acme-AnimalShelter/src/main/java/services/ApplicationService.java
@@ -2,6 +2,7 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.util.Assert;
 
 import repositories.ApplicationRepository;
 import domain.Application;
+import domain.Client;
 import domain.Pet;
 import domain.Report;
 
@@ -33,6 +35,9 @@ public class ApplicationService {
 	
 	@Autowired
 	private ReportService reportService;
+	
+	@Autowired
+	private ClientService clientService;
 
 	// Constructors
 
@@ -42,8 +47,25 @@ public class ApplicationService {
 
 	// Simple CRUD methods
 
-	public Application create() {
+	@SuppressWarnings("deprecation")
+	public Application create(Pet pet) {
 		Application res = new Application();
+		
+		Client client;
+		client = clientService.findByPrincipal();
+		res.setClient(client);
+		res.setPet(pet);
+		
+		String ticker;
+		Date date = new Date(System.currentTimeMillis() - 100);
+		
+		res.setCreateMoment(date);
+		
+		ticker = pet.getIdentifier() + date.getDay() + date.getMonth() + date.getYear();
+		
+		res.setClosed(false);
+		
+		res.setTicker(ticker);
 		
 		return res;
 
