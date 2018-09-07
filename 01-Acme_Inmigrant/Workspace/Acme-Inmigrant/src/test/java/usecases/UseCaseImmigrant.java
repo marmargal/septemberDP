c2ccbd39e38fd12cbc6f57aceb4ebdad18d86669
danirguez <32953186+danirguez@users.noreply.github.com>
2018-09-07 18:04:53 +0200

    fix: identifier pet

---
+++ 01-Acme_Inmigrant/Workspace/Acme-Inmigrant/src/test/java/usecases/UseCaseImmigrant.java
@@ -0,0 +1,17 @@
+package usecases;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class UseCaseImmigrant extends AbstractTest {
+
+}
